31b914ec785ff86958adafd47eb1c857e3deffc1
Rafael Lage Tavares <rlt.rafael@gmail.com>
2017-08-20 17:47:04 -0300

    Adding comments

--- src/App.js
+++ src/App.js
@@ -10,13 +10,19 @@ import TasksListContainer from './TasksList/TasksListContainer.js'
 
 
 const store = createStore(appReducer, applyMiddleware(logger))
-
+/**
+  * Callback to dispatch action to populate the app
+  * with tasks from the server
+  * @param data represents response data from server
+  */
 const ajaxCallback = (data) => {
     store.dispatch(populateApp(data))
 };
 API.getTodos(ajaxCallback)
 
-
+/**
+  * Represents the application
+  */
 const App = () => {
     return (
       <Provider store={store}>
--- src/api/api.js
+++ src/api/api.js
@@ -1,75 +1,75 @@
 import axios from 'axios'
 const ENDPOINT = "http://localhost:9001/"
 const API = {
-    /**
-    * Call the API and get todos' data
-    * @param callback function to call when data is fully loaded
-    */
-    getTodos(callback) {
-        axios
-            .get(ENDPOINT + 'tasks')
-            .then((response) => {
-                callback({
-                    data: response.data.tasks
-                })
-            })
-            .catch((error) => {
-                return console.log(error)
-            })
-    },
-    /**
-    * Call the API and edit todo's data
-    * @param callback function to call after response
-    * @param todo 
-    */
-    editTodo(callback, todo) {
-        axios
-            .put(ENDPOINT + 'task/update/'+todo.id+'/'+todo.title+'/'+todo.description)
-            .then((response) => {
-                callback({
-                    data: response.data.task
-                })
-            })
-            .catch((error) => {
-                return console.log(error)
-            })
-    },
-    /**
-    * Call the API and add todo's data
-    * @param callback function to call after response
-    * @param todo 
-    */
-    addTodo(callback, todo) {
-        axios
-            .post(ENDPOINT + 'task/create/'+todo.id+'/'+todo.title+'/'+todo.description)
-            .then((response) => {
-                callback({
-                    data: response.data.task
-                })
-            })
-            .catch((error) => {
-                return console.log(error)
-            })
-    },
+  /**
+  * Call the API and get todos' data
+  * @param callback function to call when data is fully loaded
+  */
+  getTodos(callback) {
+    axios
+      .get(ENDPOINT + 'tasks')
+      .then((response) => {
+        callback({
+          data: response.data.tasks
+        })
+      })
+      .catch((error) => {
+        return console.log(error)
+      })
+  },
+  /**
+  * Call the API and edit todo's data
+  * @param callback function to call after response
+  * @param todo 
+  */
+  editTodo(callback, todo) {
+    axios
+      .put(ENDPOINT + 'task/update/' + todo.id + '/' + todo.title + '/' + todo.description)
+      .then((response) => {
+        callback({
+          data: response.data.task
+        })
+      })
+      .catch((error) => {
+        return console.log(error)
+      })
+  },
+  /**
+  * Call the API and add todo's data
+  * @param callback function to call after response
+  * @param todo 
+  */
+  addTodo(callback, todo) {
+    axios
+      .post(ENDPOINT + 'task/create/' + todo.id + '/' + todo.title + '/' + todo.description)
+      .then((response) => {
+        callback({
+          data: response.data.task
+        })
+      })
+      .catch((error) => {
+        return console.log(error)
+      })
+  },
 
-    /**
-    * Call the API and remove todo's data
-    * @param callback function to call after response
-    * @param todo 
-    */
-    removeTodo(callback, todo) {
-        axios
-            .delete(ENDPOINT + 'task/delete/'+todo.id)
-            .then((response) => {
-                console.log(response)
-                callback({
-                    data: response.data.task
-                })
-            })
-            .catch((error) => {
-                return console.log(error)
-            })
-    },
+  /**
+  * Call the API and remove todo's data
+  * @param callback function to call after response
+  * @param todo 
+  */
+  removeTodo(callback, todo) {
+    axios
+      .delete(ENDPOINT + 'task/delete/' + todo.id)
+      .then((response) => {
+        console.log(response)
+        callback({
+          data: response.data.task
+        })
+      })
+      .catch((error) => {
+        return console.log(error)
+      })
+  },
 }
 
 export default API
